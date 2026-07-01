491d63cdc6a212977643ff32f1f999f603002ca6
Kritsada <ninep9p2006@gmail.com>
2026-07-01 10:17:10 +0700

    feat: add create failure page with fetched category and emotion data

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,6 +1,8 @@
 import { env } from "@/env";
 import CreateFailurePage from "@/components/dashboard/CreateFailurePage";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
 
   const categoryList = await fetch(`${env.NEXT_PUBLIC_APP_URL}/api/category`, {
